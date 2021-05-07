bb7df5f6b4074aa0056e774fee60bccd448a6be7
ATaoyoyo <1029187998@qq.com>
2021-05-07 23:23:57 +0800

    引入commit校验

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,15 @@
 module.exports = {
+  lintOnSave: true,
+  runtimeCompiler: true,
+
+  chainWebpack: config => {
+    // 修复HMR
+    config.resolve.symlinks(true)
+  },
+
   devServer: {
     open: true,
-    port: 8090
+    port: 8090,
+    hotOnly: true
   }
 }
